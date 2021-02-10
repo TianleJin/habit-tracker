c9a223850433ed8b3b30db6d803f52c3bf7eeb5d
TianleJin <tianlejin99@gmail.com>
2021-02-11 10:52:05 +1100

    added basic database and login mechanism

--- index.py
+++ index.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash, get_flashed_messages
+from flask import Flask, render_template, redirect, url_for, request, session, flash, get_flashed_messages, g
+import sqlite3
+
 app = Flask(__name__)
 app.secret_key = 'abcdefghijklmnopqrstuvwxyz'
+DATABASE = 'user.db'
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(DATABASE)
+        # g.db.execute('''CREATE TABLE users (
+        #                 username TEXT NOT NULL PRIMARY KEY,
+        #                 password TEXT NOT NULL
+        #             );''')
+    return g.db
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def index():
@@ -15,18 +33,35 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # flash('Your username or password is incorrect.')
-        session['username'] = request.form['username']
-        return redirect(url_for('home'))
+        cur = get_db().cursor()
+        cur.execute('SELECT * FROM users WHERE username = ?', (request.form['username'], ))
+        res = cur.fetchone()
+        if res and res[1] == request.form['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('home'))
+        flash('Your username or password is incorrect.')
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        # flash('That username has already been taken.')
-        # flash('Your passwords do not match.')
-        session['username'] = request.form['username']
-        return redirect(url_for('home'))
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM users WHERE username = ?', (request.form['username'], ))
+        res = cur.fetchone()
+        if res:
+            flash('That username has already been taken.')
+        elif not request.form['username']:
+            flash('You must provide a username.')
+        elif not request.form['password']:
+            flash('You must provide a password.')
+        elif request.form['password'] != request.form['confirm']:
+            flash('Your passwords do not match.')
+        else:
+            cur.execute('INSERT INTO users (username, password) VALUES (?, ?)', (request.form['username'], request.form['password']))
+            conn.commit()
+            session['username'] = request.form['username']
+            return redirect(url_for('home'))
     return render_template('register.html')
 
 @app.route('/logout')
