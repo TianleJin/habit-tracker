cabefa2b07e8561972c0a365871b69f6de8ef1f0
TianleJin <tianlejin99@gmail.com>
2021-02-28 21:42:32 +1100

    added confirmation modal and info modal to habit page

--- static/js/habit.js
+++ static/js/habit.js
@@ -1,18 +1,15 @@
-function removeHabit(habit_id, habit_name) {
-    if (confirm(`Do you wish to remove habit '${habit_name}'? This action will be permanent.`)) {
-        $.ajax({
-            url: `habit/${habit_id}`,
-            type: 'DELETE',
-            success: function(result) {
-                console.log(result);
-                document.getElementById(`habit-${habit_id}`).style.opacity = 0;
-                setTimeout(function () {
-                    document.getElementById(`habit-${habit_id}`).style.display = "none";
-                }, 300);
-            },
-            error: function(XMLHttpRequest, textStatus, errorThrown) { 
-                alert("Status: " + textStatus); alert("Error: " + errorThrown); 
-            }
-        });
-    }
+function removeHabit(habit_id) {
+    $.ajax({
+        url: `habit/${habit_id}`,
+        type: 'DELETE',
+        success: function(result) {
+            document.getElementById(`habit-${habit_id}`).style.opacity = 0;
+            setTimeout(function() {
+                document.getElementById(`habit-${habit_id}`).style.display = "none";
+            }, 500);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) { 
+            alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+        }
+    });
 }
