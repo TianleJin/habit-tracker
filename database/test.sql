74d04ea2d9b9e887b9d0867d83ffd2ba1392d186
TianleJin <tianlejin99@gmail.com>
2021-03-08 18:27:17 +1100

    added test.sql

---
+++ database/test.sql
@@ -0,0 +1,43 @@
+PRAGMA foreign_keys = ON;
+
+INSERT INTO record (record_date, habit_id, status) VALUES ("02-03-2021", 1, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("02-03-2021", 2, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("02-03-2021", 3, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("02-03-2021", 4, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("02-03-2021", 5, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("02-03-2021", 6, 0);
+
+INSERT INTO record (record_date, habit_id, status) VALUES ("03-03-2021", 1, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("03-03-2021", 2, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("03-03-2021", 3, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("03-03-2021", 4, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("03-03-2021", 5, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("03-03-2021", 6, 0);
+
+INSERT INTO record (record_date, habit_id, status) VALUES ("04-03-2021", 1, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("04-03-2021", 2, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("04-03-2021", 3, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("04-03-2021", 4, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("04-03-2021", 5, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("04-03-2021", 6, 1);
+
+INSERT INTO record (record_date, habit_id, status) VALUES ("05-03-2021", 1, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("05-03-2021", 2, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("05-03-2021", 3, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("05-03-2021", 4, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("05-03-2021", 5, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("05-03-2021", 6, 1);
+
+INSERT INTO record (record_date, habit_id, status) VALUES ("06-03-2021", 1, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("06-03-2021", 2, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("06-03-2021", 3, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("06-03-2021", 4, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("06-03-2021", 5, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("06-03-2021", 6, 1);
+
+INSERT INTO record (record_date, habit_id, status) VALUES ("07-03-2021", 1, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("07-03-2021", 2, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("07-03-2021", 3, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("07-03-2021", 4, 0);
+INSERT INTO record (record_date, habit_id, status) VALUES ("07-03-2021", 5, 1);
+INSERT INTO record (record_date, habit_id, status) VALUES ("07-03-2021", 6, 0);
